eb9cf1f66802967560f73667c678db67fd52fd51
silvester <silvesterlhwali123@gmail.com>
2024-01-29 17:45:23 +0800

    feat: contact example

---
+++ composables/useSchema.ts
@@ -0,0 +1,39 @@
+import { object, string, boolean, array, any, union, number } from "zod";
+import { toTypedSchema } from "@vee-validate/zod";
+
+export default function () {
+  /**
+   * catatan jika type string harus diikuti dengan .min(1, "Name is required")
+   * karena ="" adalah string
+   *
+   */
+
+  const contactSchema = toTypedSchema(
+    object({
+      name: string({
+        required_error: "Name is required",
+      })
+        .trim()
+        .min(1, "Name is required"),
+      email: string({
+        required_error: "Email is required",
+      })
+        .trim()
+        .email("Invalid email address"),
+      subject: string({
+        required_error: "Subject is required",
+      })
+        .trim()
+        .min(1, "Subject is required"),
+      message: string({
+        required_error: "Message is required",
+      })
+        .trim()
+        .min(1, "Message is required"),
+    })
+  );
+
+  return {
+    contactSchema,
+  };
+}
