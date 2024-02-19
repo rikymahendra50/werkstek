242cd996e7eac76e5972156f8ce47361cc53be5e
silvester <silvesterlhwali123@gmail.com>
2024-02-19 18:24:51 +0800

    forgot password

---
+++ components/Form/Group.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="grid grid-cols-1 gap-1 text-left">
+        <label :for="props.name" class="tracking-wide font-medium">
+            {{ props.label }}
+        </label>
+        <div class="form-description" v-if="$slots.descriptionTop">
+            <slot name="descriptionTop" />
+        </div>
+        <slot />
+        <div class="form-description" v-if="$slots.descriptionBottom">
+            <slot name="descriptionBottom" />
+        </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+const props = defineProps({
+    /**
+     * input label
+     * @type {string}
+     * @example
+     */
+    label: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+defineOptions({
+    inheritAttrs: false,
+});
+</script>
+  
+<style lang="scss" scoped></style>
+  
