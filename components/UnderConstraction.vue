d4e302df2521b0e165085ab6461ab58be678c5b0
rikymahendra50 <rikymahendra50@gmail.com>
2024-06-12 13:35:13 +0800

    add contact under construction

--- components/UnderConstraction.vue
+++ components/UnderConstraction.vue
@@ -9,9 +9,19 @@
         class="w-[300px]"
       />
     </div>
-    <p class="text-center text-2xl leading-normal">
+    <p class="text-center text-xl sm:text-2xl leading-normal">
       Sorry, Official Werkstek website right now is under construction, <br />
       Please comeback later.
     </p>
+    <div
+      class="text-lg leading-normal mt-5 text-start bg-white w-[80%] lg:w-[40%] rounded-[20px] border py-2 shadow-sm"
+    >
+      <div class="text-center text-lg flex flex-col gap-2">
+        <p class="text-xl font-bold">Contact Details</p>
+        <a href="tel:085-0290598">085-0290598</a>
+        <p>or</p>
+        <a href="mailto:info@werkstek.nl">info@werkstek.nl</a>
+      </div>
+    </div>
   </section>
 </template>
