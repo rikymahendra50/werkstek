28eb18c8202767e9fc98db81d179e2918de6fc21
rikymahendra50 <rikymahendra50@gmail.com>
2024-08-21 15:00:55 +0800

    test

--- components/EachTestimony.vue
+++ components/EachTestimony.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative text-left px-5 py-5 rounded-[16px] flex flex-col box-shadow2 my-10 h-[320px] lg:h-[300px] mx-1"
+    class="relative text-left px-5 py-5 rounded-[16px] flex flex-col box-shadow2 my-10 h-fit lg:h-[320px] mx-1"
   >
     <div class="flex flex-col justify-between gap-2 h-full">
       <!-- /images/icon-werstek.svg -->
@@ -13,12 +13,12 @@
         class="text-[12px] sm:text-[14px] 2xl:text-[18px] text-[#3D3D3D] leading-normal"
         v-html="description"
       ></p>
-      <div class="grid grid-cols-2 pt-2 border-t">
-        <div class="flex flex-col text-sm gap-1 text-[10px]">
+      <div class="grid grid-cols-12 pt-2 border-t">
+        <div class="flex flex-col text-sm gap-1 text-[10px] col-span-8">
           <p class="font-bold text-[12px]">{{ testimonyName }}</p>
           <p class="text-sm">{{ position }}</p>
         </div>
-        <div class="flex items-center justify-end">
+        <div class="flex items-center justify-end col-span-4">
           <Icon
             name="ic:round-star"
             class="text-[#da9a3b] w-4"
