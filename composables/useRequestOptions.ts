dbc1311663b24fb009a96c4228f63c95a0825a75
rikymahendra50 <rikymahendra50@gmail.com>
2024-12-06 09:43:41 +0800

    test

--- composables/useRequestOptions.ts
+++ composables/useRequestOptions.ts
@@ -8,22 +8,24 @@ export default function () {
     watch: true,
   });
 
-  async function clearCredential() {
-    if (process.client) {
-      useCookie("auth-token").value = null;
-      window.location.replace("/admin/sign-in");
-    }
-  }
+  // async function clearCredential() {
+  //   if (process.client) {
+  //     useCookie("auth-token").value = null;
+  //     window.location.replace("/admin/sign-in");
+  //   }
+  // }
+
   function onRequest(context: any) {
     context.options.headers = context.options.headers || {};
     context.options.headers.authorization = credential.value?.token
       ? "Bearer " + credential.value?.token
       : "";
   }
+
   function onResponseError(context: any) {
-    if (context.response.status === 401) {
-      return clearCredential();
-    }
+    // if (context.response.status === 401) {
+    //   return clearCredential();
+    // }
   }
 
   const requestOptions = {
@@ -31,6 +33,7 @@ export default function () {
     onRequest: (contex: any) => onRequest(contex),
     onResponseError: (context: any) => onResponseError(context),
   };
+
   return {
     requestOptions,
   };
